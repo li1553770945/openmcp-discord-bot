66f1027768957e091b2099b6cc899ae2c01b000b
li1553770945 <1553770945@qq.com>
2025-05-10 01:04:40 +0800

    feat:添加构建和运行脚本

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 如果 pid.txt 存在，读取其中的 PID 并终止对应的进程
+if [ -f pid.txt ]; then
+    old_pid=$(cat pid.txt)
+    if ps -p $old_pid > /dev/null 2>&1; then
+        echo "Killing previous process with PID $old_pid"
+        kill $old_pid
+    fi
+    rm pid.txt
+fi
+
+chmod +x ./openmcp-discord
+
+# 使用 nohup 运行 main.py 并将输出重定向到 log 文件
+nohup ./openmcp-discord  > /dev/null 2>&1 &
+
+# 将新的 PID 保存到 pid.txt
+echo $! > pid.txt
+echo "New process started with PID $(cat pid.txt)"
